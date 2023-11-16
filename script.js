c52071300dd39d84365c1a6a7e48696bf8bf4c84
Mhivelin <marius.hivelin@yahoo.fr>
2023-11-16 16:33:29 +0100

    up

--- script.js
+++ script.js
@@ -3,78 +3,129 @@
 
 // ######################################################### affichage des employés #########################################################
 
-// definition des variables globales
-let bureaux = [];
-let employes = [];
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+function generateTable(bureaux) {
+
+    largeur = 0;
+    longueur = 0;
+
+    // calculer la largeur et la longueur du tableau
+
+    // largeur = nombre de colonnes
+    largeur = bureaux[0].length;
+
+    // longueur = nombre de lignes
+    longueur = bureaux.length;
+
+    //console.log("largeur : " + largeur);
+    //console.log("longueur : " + longueur);
+
+    // recupere le tableau <table class="table table-bordered">
+
+
+    let tableau = document.getElementById("tableau");
+
+    // creation des lignes et des colonnes
+    for (let i = 0; i < longueur; i++) {
+        let ligne = document.createElement("tr");
+        for (let j = 0; j < largeur; j++) {
+            let colonne = document.createElement("td");
+
+            if (bureaux[i][j] != '') {
+                // on ajoute l'id de la place
+                colonne.id = "place" + bureaux[i][j];
+                // on ajoute la classe place
+                colonne.classList.add("place");
+
+            }
+            colonne.classList.add("x" + j);
+            colonne.classList.add("y" + i);
+
+
+
+            ligne.appendChild(colonne);
+        }
+        tableau.appendChild(ligne);
+
+
+    }
+
+}
 
 
 
 // Cette fonction récupère et affiche la liste des employés dans la table
-function afficherListeEmployes(employes) {
-    const tableau = document.getElementById("tableau");
+function placerEmployes(employes) {
+    for (let i = 0; i < employes.length; i++) {
 
 
 
-    employes.forEach((employe, index) => {
-        const newRow = document.createElement("tr");
-        newRow.innerHTML = `
-    <td>${employe.prenom}</td>
-    <td><img src="${employe.image}" width="50px" /></td>
-    <td><button type="button" class="btn btn-danger" onclick="supprimerEmploye(${index})">Supprimer</button></td>
-    `;
-        tableau.appendChild(newRow);
-    });
-}
+        let id = "place" + employes[i].placeActuelle;
+
+        console.log("id : " + id);
+
+        placeTableau = document.getElementById(id);
+
+        console.log("placeTableau : " + placeTableau);
+
+        placeTableau.innerHTML = "<img src=" + employes[i].image + " class='img-fluid' alt='employe' width='100' height='100'>";
 
 
-// Fonction pour remplir le tableau des employés avec des employés vides
-function fillEmptyEmployees() {
-    const emptyEmployee = {
-        "prenom": "",
-        "image": ""
-    };
-
-
-    nbplaces = 0;
-
-    for (let i = 0; i < bureaux.length; i++) {
-        nbplaces += bureaux[i].places;
-    }
-
-
-
-    while (employes.length < nbplaces) {
-        employes.push(emptyEmployee);
     }
 
 
 }
 
 
+function afficherEmployes(employes) {
+    // ajoute les employés dans la liste des employés
+    /* | Prénom	 | Image	| Place actuelle | Action |
+    |-----------|-----------|----------------|--------|
+    | Adele	 | Adele.png | 1				 | Supprimer |
 
-function generateTable(bureauIndex) {
-    let tableHTML = "<table class='table table-bordered'>";
-    let bureau = bureaux[bureauIndex];
-    for (let j = 0; j < 2; j++) {
-        tableHTML += "<tr>";
-        for (let k = 0; k < bureau.places / 2; k++) {
-            let employe = employes.pop();
-            if (employe.image == "") {
-                tableHTML += "<td class=employee-cell height='100px width='100px'></td>";
-            } else {
-                tableHTML += "<td class=employee-cell><img src='" + employe.image + "' height='100px' /></td>";
-            }
-        }
-        tableHTML += "</tr>";
-    }
-    tableHTML += "</table>";
-    return tableHTML;
+    */
+    /*
+        let listeEmployes = document.getElementById("tableauEmploye");
+    
+        for (let i = 0; i < employes.length; i++) {
+    
+            let ligne = document.createElement("tr");
+    
+            let colonnePrenom = document.createElement("td");
+            colonnePrenom.innerHTML = employes[i].prenom;
+            ligne.appendChild(colonnePrenom);
+    
+            let colonneImage = document.createElement("td");
+            colonneImage.innerHTML = "<img src=" + employes[i].image + " class='img-fluid' alt='employe' width='100' height='100'>";
+            ligne.appendChild(colonneImage);
+    
+            let colonnePlaceActuelle = document.createElement("td");
+            colonnePlaceActuelle.innerHTML = employes[i].placeActuelle;
+            ligne.appendChild(colonnePlaceActuelle);
+    
+            let colonneAction = document.createElement("td");
+            colonneAction.innerHTML = "<button type='button' class='btn btn-danger' onclick='supprimerEmploye(" + i + ")'>Supprimer</button>";
+            ligne.appendChild(colonneAction);
+    
+            listeEmployes.appendChild(ligne);
+    
+    
+    
+        }*/
 }
 
 
@@ -105,7 +156,7 @@ function ajouterEmploye() {
     formData.append("prenom", prenom);
     formData.append("image", imageFile);
 
-    fetch("http://54.38.32.241:3000/addEmployee", {
+    fetch("http://192.168.75.154:3000/addEmployee", {
         method: "POST",
         body: formData,
     })
@@ -121,7 +172,7 @@ function ajouterEmploye() {
             }
         })
         .catch(error => {
-            console.error("Erreur lors de la requête : ", error);
+            //console.error("Erreur lors de la requête : ", error);
         });
 
     // actualiser la page
@@ -137,10 +188,10 @@ document.getElementById("ajout").addEventListener("click", ajouterEmploye);
 function supprimerEmploye(index) {
 
 
-    console.log("supprimerEmploye");
+    //console.log("supprimerEmploye");
 
     // La route '/supprimerEmploye' pour supprimer un employé
-    fetch('http://54.38.32.241:3000/supprimerEmploye', {
+    fetch('http://192.168.75.154:3000/supprimerEmploye', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -149,11 +200,11 @@ function supprimerEmploye(index) {
     })
         .then(response => response.json())
         .then(result => {
-            console.log('Employé supprimé avec succès !');
+            //console.log('Employé supprimé avec succès !');
             // Vous pouvez mettre à jour la liste des employés et les tables ici si nécessaire
         })
         .catch(error => {
-            console.error('Erreur lors de la suppression de l\'employé :', error);
+            //console.error('Erreur lors de la suppression de l\'employé :', error);
         });
 
     // actualiser la page
@@ -167,7 +218,7 @@ function supprimerEmploye(index) {
 
 function shuffle() {
     // appel de l'api pour melanger les employés
-    fetch('http://54.38.32.241:3000/shuffleEmployees', {
+    fetch('http://192.168.75.154:3000/shuffleEmployees', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -176,54 +227,127 @@ function shuffle() {
     })
         .then(response => response.json())
         .then(result => {
-            console.log('Employés mélangés avec succès !');
+            //console.log('Employés mélangés avec succès !');
             // Vous pouvez mettre à jour la liste des employés et les tables ici si nécessaire
         })
         .catch(error => {
-            console.error('Erreur lors du mélange des employés :', error);
+            //console.error('Erreur lors du mélange des employés :', error);
         });
 
 
+
+
+
+
+
 }
+
+// Cette fonction est appelée lorsque vous cliquez sur "Mélanger"
+document.getElementById("melange").addEventListener("click", shuffle);
 
 
 
 // ######################################################### chargement des employés et des bureaux #########################################################
 
 
-// Chargez la liste des employés existante depuis le serveur
-fetch('http://54.38.32.241:3000/getEmployees')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Employés chargés avec succès : ', data);
-        afficherListeEmployes(data);
+let bureaux = [];
 
-        employes = data;
-
-        // recupere les bureaux
-        fetch('http://54.38.32.241:3000/getBureaux')
+function fetchBureaux() {
+    try {
+        fetch('http://192.168.75.154:3000/getBureaux')
             .then(response => response.json())
             .then(data => {
-                console.log('Bureaux chargés avec succès : ', data);
                 bureaux = data;
-                // remplir les employés vides
-                fillEmptyEmployees();
-                // melanger les employés
-                shuffle(employes);
-                // generer les tables
-                for (let i = 0; i < bureaux.length; i++) {
-                    let tableHTML = generateTable(i);
-                    document.getElementById("bureau" + (i + 1) + "Table").innerHTML = tableHTML;
-                }
-            })
-            .catch(error => {
-                console.error('Erreur de chargement des bureaux :', error);
+                console.log(bureaux);
+                generateTable(bureaux);
+            });
+
+    } catch (error) {
+        console.error("Une erreur s'est produite : ", error);
+    }
+}
+
+fetchBureaux();
+
+
+
+
+
+
+let employes = [];
+
+function fetchEmployees() {
+    try {
+        fetch('http://192.168.75.154:3000/getEmployees')
+            .then(response => response.json())
+            .then(data => {
+                employes = data;
+                console.log(employes);
+                placerEmployes(employes);
+                afficherEmployes(employes);
             });
 
 
 
-        // Vous pouvez également mettre à jour les tables ici si nécessaire
+        // Vous pouvez effectuer d'autres opérations avec les données ici
+    } catch (error) {
+        console.error("Une erreur s'est produite : ", error);
+    }
+}
+
+fetchEmployees();
+
+
+
+// ######################################################### ajout et suppression des bureaux #########################################################
+
+// Cette fonction est appelée lorsque vous cliquez sur une case du tableau qui n'est pas un bureau
+
+
+function toggleBureau(x, y) {
+
+
+
+    // La route '/addBureau' pour ajouter un bureau
+    fetch('http://192.168.75.154:3000/toggleBureau', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ places: 1, x: x, y: y }),
     })
-    .catch(error => {
-        console.error('Erreur de chargement des employés :', error);
+        .then(response => response.json())
+        .then(result => {
+            //console.log('Bureau ajouté avec succès !');
+            // Vous pouvez mettre à jour la liste des bureaux et les tables ici si nécessaire
+        })
+        .catch(error => {
+            //console.error('Erreur lors de l\'ajout du bureau :', error);
+        });
+
+    // actualiser la page
+    location.reload();
+
+}
+
+// declanchement de la fonction ajouterBureau
+
+// liste des cases
+let cases = document.getElementsByClassName("place");
+
+for (let i = 0; i < cases.length; i++) {
+    console.log(cases[i]);
+    cases[i].addEventListener("click", function () {
+
+
+
+        // recuperation des coordonnées de la case
+        let x = this.classList[1].slice(1);
+        let y = this.classList[2].slice(1);
+
+        toggleBureau(x, y);
+
     });
+}
+
+
--- serveur/server.js
+++ serveur/server.js
@@ -85,6 +85,10 @@ app.get('/getEmployees', (req, res) => {
 });
 
 app.get('/getBureaux', (req, res) => {
+
+    // affichage console
+    console.log("getBureaux");
+
     fs.readFile(__dirname + '/bureaux.json', (err, data) => {
         if (err) {
             console.log(err);
@@ -92,7 +96,86 @@ app.get('/getBureaux', (req, res) => {
             return;
         }
         const bureaux = JSON.parse(data);
-        res.json(bureaux);
+
+
+
+        /*
+        [
+  {
+    '1': { x: 0, y: 0 },
+    '2': { x: 0, y: 1 },
+    '3': { x: 1, y: 0 },
+    '4': { x: 1, y: 1 },
+    '5': { x: 0, y: 5 },
+    '6': { x: 0, y: 6 },
+    '7': { x: 1, y: 5 },
+    '8': { x: 1, y: 6 },
+    '9': { x: 0, y: 9 },
+    '10': { x: 0, y: 10 },
+    '11': { x: 1, y: 9 },
+    '12': { x: 1, y: 10 },
+    '13': { x: 5, y: 9 },
+    '14': { x: 5, y: 10 },
+    '15': { x: 6, y: 9 },
+    '16': { x: 6, y: 10 },
+    '17': { x: 7, y: 9 },
+    '18': { x: 7, y: 10 },
+    '19': { x: 8, y: 9 },
+    '20': { x: 8, y: 10 }
+  }
+]
+ */
+
+
+        xMax = 0;
+        yMax = 0;
+
+        // recuperation des valeurs max de x et y
+        for (let i = 0; i < bureaux.length; i++) {
+            for (const [key, value] of Object.entries(bureaux[i])) {
+                if (value.x > xMax) {
+                    xMax = value.x;
+                }
+                if (value.y > yMax) {
+                    yMax = value.y;
+                }
+            }
+            yMax++;
+            xMax++;
+        }
+
+
+        console.log("xMax : " + xMax);
+        console.log("yMax : " + yMax);
+
+        let listeBureaux = [];
+
+        for (let i = 0; i < yMax; i++) {
+            listeBureaux.push([]);
+
+            for (let j = 0; j < xMax; j++) {
+                console.log("i : " + i);
+                console.log("j : " + j);
+
+
+                // on verifie si le bureau existe
+                let bureauExiste = "";
+                for (let k = 0; k < bureaux.length; k++) {
+                    for (const [key, value] of Object.entries(bureaux[k])) {
+                        if (value.x == j && value.y == i) {
+                            bureauExiste = key;
+                            console.log("bureauExiste : " + bureauExiste);
+                        }
+                    }
+                }
+
+                listeBureaux[i].push(bureauExiste);
+
+            }
+        }
+
+        res.json(listeBureaux);
+
     });
 });
 
@@ -125,73 +208,162 @@ app.listen(3000, () => {
 
 // /shuffleEmployees
 
-app.post('/shuffleEmployees', () => {
-    // recuperer les employes dans le json
+app.post('/shuffleEmployees', (req, res) => { // Ajoutez la requête et la réponse en tant que paramètres
+
+    console.log("shuffleEmployees");
+
+    // Récupérer les employés dans le JSON
     fs.readFile(__dirname + '/employes.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.json({ success: false, error: 'Erreur de lecture des employés' }); // Retournez une réponse avec une indication d'erreur
+        }
+        const employes = JSON.parse(data);
+
+        // Récupérer les bureaux dans le JSON
+        fs.readFile(__dirname + '/bureaux.json', (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json({ success: false, error: 'Erreur de lecture des bureaux' }); // Retournez une réponse avec une indication d'erreur
+            }
+            const bureaux = JSON.parse(data);
+
+            console.log("bureaux : " + bureaux);
+            console.log(bureaux);
+
+
+
+
+
+            // Melanger les indices de placeActuelle des employes
+            let places = [];
+            // parcourir les bureaux
+            for (const [key, value] of Object.entries(bureaux)) {
+                // parcourir les places
+                for (const [key2, value] of Object.entries(bureaux[key])) {
+                    //console.log("key : " + key);
+                    //console.log("value : " + value);
+                    places.push(key2);
+                }
+            }
+            for (let i = 0; i < places.length; i++) {
+                let randomIndex = Math.floor(Math.random() * places.length);
+                let temp = places[i];
+                places[i] = places[randomIndex];
+                places[randomIndex] = temp;
+            }
+
+            // Affecter les employes aux bureaux
+            for (let i = 0; i < employes.length; i++) {
+                employes[i].placeActuelle = places[i];
+            }
+
+            // Écrire les employes dans le JSON
+            fs.writeFile(__dirname + '/employes.json', JSON.stringify(employes), (err) => {
+                if (err) {
+                    console.log(err);
+                    return res.json({ success: false, error: 'Erreur d\'écriture des employés' }); // Retournez une réponse avec une indication d'erreur
+                }
+
+                return res.json({ success: true, message: 'Les employés ont été attribués aux bureaux avec succès.' }); // Retournez une réponse de succès
+            });
+        });
+    });
+});
+
+
+
+
+// /addBureau
+
+app.post('/toggleBureau', (req, res) => {
+    // lire le json des bureaux
+    fs.readFile(__dirname + '/bureaux.json', (err, data) => {
         if (err) {
             console.log(err);
             res.json({ success: false });
             return;
         }
-        const employes = JSON.parse(data);
+        const bureaux = JSON.parse(data);
 
-        // recuperer les bureaux dans le json
-        fs.readFile(__dirname + '/bureaux.json', (err, data) => {
+        x = req.body.x;
+        y = req.body.y;
+
+        // verifier si le bureau existe
+        let bureauExiste = false;
+
+        for (let i = 0; i < bureaux.length; i++) {
+            for (const [key, value] of Object.entries(bureaux[i])) {
+                if (value.x == x && value.y == y) {
+                    bureauExiste = true;
+                    break;
+                }
+            }
+        }
+
+        // si le bureau existe, on le supprime
+        if (bureauExiste) {
+            console.log("bureauExiste");
+
+        } else {
+            console.log("bureauExistePas");
+        }
+
+        /*
+        
+                // trouver le plus petit id disponible
+                let id = 1;
+                for (let i = 0; i < bureaux.length; i++) {
+                    if (bureaux[i].id == id) {
+                        id++;
+                    }
+                }
+                // ajouter le nouveau bureau
+                bureaux.push({ id: id, places: req.body.places, x: req.body.x, y: req.body.y });
+                // ecrire le json des bureaux
+                fs.writeFile(__dirname + '/bureaux.json', JSON.stringify(bureaux), (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.json({ success: false });
+                        return;
+                    }
+                    res.json({ success: true });
+                });*/
+    }
+    );
+}
+);
+
+
+
+
+
+// /deleteBureau
+
+app.post('/deleteBureau', (req, res) => {
+    // lire le json des bureaux
+    fs.readFile(__dirname + '/bureaux.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({ success: false });
+            return;
+        }
+        const bureaux = JSON.parse(data);
+        // supprimer le bureau
+        for (let i = 0; i < bureaux.length; i++) {
+            if (bureaux[i].id == req.body.id) {
+                bureaux.splice(i, 1);
+            }
+        }
+        // ecrire le json des bureaux
+        fs.writeFile(__dirname + '/bureaux.json', JSON.stringify(bureaux), (err) => {
             if (err) {
                 console.log(err);
                 res.json({ success: false });
                 return;
             }
-            const bureaux = JSON.parse(data);
-
-            //compter le nombre de place dans les bureaux
-            let nbPlaces = 0;
-            for (let i = 0; i < bureaux.length; i++) {
-                nbPlaces += bureaux[i].places;
-            }
-            // ajouter les employes vides pour completer les places
-            for (let i = 0; i < nbPlaces - employes.length; i++) {
-                employes.push({ prenom: "", image: "" });
-            }
-
-            /*     {
-        "id": 1,
-        "prenom": "Adele",
-        "image": "serveur/images/Adele.png",
-        "placeActuelle": 1
-    }, */
-            // melanger les index de place des employes
-            let indexPlace = [];
-            for (let i = 0; i < nbPlaces; i++) {
-                indexPlace.push(i);
-            }
-
-            for (let i = 0; i < indexPlace.length; i++) {
-                let randomIndex = Math.floor(Math.random() * indexPlace.length);
-                let temp = indexPlace[i];
-                indexPlace[i] = indexPlace[randomIndex];
-                indexPlace[randomIndex] = temp;
-            }
-
-            // affecter les places aux employes
-            for (let i = 0; i < employes.length; i++) {
-                employes[i].placeActuelle = indexPlace[i];
-            }
-
-
-
-
-            // ecrire les employes dans le json
-            fs.writeFile(__dirname + '/employes.json', JSON.stringify(employes), (err) => {
-                if (err) {
-                    console.log(err);
-                    res.json({ success: false });
-                    return;
-                }
-                res.json({ success: true });
-            });
-        }
-        );
+            res.json({ success: true });
+        });
     }
     );
 }
